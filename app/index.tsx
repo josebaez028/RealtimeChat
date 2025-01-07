1ecd2cb74c959353b75395bfdc0de13b039cb6b2
josebaez028 <josebaez028@gmail.com>
2025-01-07 17:18:31 -0400

    Primer commit

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const index = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="">RootLayout</Text>
+      <Link className="py-2 px-3 my-5 bg-blue-500 text-white"
+      href={"/Chat"}>
+        Go to Chat
+
+      </Link>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
